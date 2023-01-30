b3d1ed655d0c1df772eb731f99909281fe869ba1
italo-baciliere <italoluciano57@gmail.com>
2023-01-30 12:06:14 -0300

    Iniciando com JWT

---
+++ api/src/main/java/br/com/voll_med/api/domain/usuario/DadosAutenticacao.java
@@ -0,0 +1,10 @@
+package br.com.voll_med.api.domain.usuario;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DadosAutenticacao (
+    @NotBlank
+    String login,
+    @NotBlank
+    String senha
+){}
--- api/src/main/java/br/com/voll_med/api/infra/security/SecurityConfigurations.java
+++ api/src/main/java/br/com/voll_med/api/infra/security/SecurityConfigurations.java
@@ -2,9 +2,13 @@ package br.com.voll_med.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +22,22 @@ public class SecurityConfigurations {
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and().build();
     };
+
+    @Bean
+    /*
+     * O @Bean serve para exportar uma classe para o Spring, fazendo com que
+     * ele consiga carregá-la e realize a sua injeção de depedência em outras
+     * classes.
+     * 
+    */
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
