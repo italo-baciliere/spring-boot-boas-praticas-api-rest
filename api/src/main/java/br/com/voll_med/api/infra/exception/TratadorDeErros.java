d28e660eb44c876ca2e1855f30e84d4939ea0941
Ítalo Baciliere <43049564+italo-baciliere@users.noreply.github.com>
2023-03-05 10:46:08 -0300

    Tentativa de rodar no mac
    Tentei rodar o projeto no macbook, porém sem sucesso.
    Acabei tendo que fazer o upload do projeto pelo hub mesmo.

--- api/src/main/java/br/com/voll_med/api/infra/exception/TratadorDeErros.java
+++ api/src/main/java/br/com/voll_med/api/infra/exception/TratadorDeErros.java
@@ -5,7 +5,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException.BadRequest;
 
 import jakarta.persistence.EntityNotFoundException;
 
